7f0cf8fd0b13ef61a05437df76a07e32f6440bbb
Saed Sousa <saed.sousa@hotmail.com>
2024-06-20 11:00:34 -0300

    Create BackEnd Project

---
+++ BackEnd/src/test/java/br/dev/saed/dscommerce/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.saed.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
