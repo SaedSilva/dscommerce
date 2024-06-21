c7552a0d926f87ae86c364045ff0ce85cd257bfd
Saed Sousa <saed.sousa@hotmail.com>
2024-06-21 17:06:12 -0300

    Product Controller, Service

--- BackEnd/src/main/java/br/dev/saed/dscommerce/controllers/ProductController.java
+++ BackEnd/src/main/java/br/dev/saed/dscommerce/controllers/ProductController.java
@@ -3,10 +3,14 @@ package br.dev.saed.dscommerce.controllers;
 import br.dev.saed.dscommerce.dto.ProductDTO;
 import br.dev.saed.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 // Anotações que ficam acima da declaração da classe
 @RestController // Indica que a classe é um controlador REST
@@ -20,5 +24,10 @@ public class ProductController {
     @GetMapping(value = "/{id}") // Indica que o método responde a requisições GET em /products/{id}
     public ProductDTO findById(@PathVariable Long id) { // @PathVariable indica que o parâmetro vem da URL
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- BackEnd/src/main/java/br/dev/saed/dscommerce/services/ProductService.java
+++ BackEnd/src/main/java/br/dev/saed/dscommerce/services/ProductService.java
@@ -4,10 +4,10 @@ import br.dev.saed.dscommerce.dto.ProductDTO;
 import br.dev.saed.dscommerce.entities.Product;
 import br.dev.saed.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service // Indica que a classe é um componente de serviço
 public class ProductService {
@@ -17,9 +17,13 @@ public class ProductService {
 
     @Transactional(readOnly = true) // Indica que é uma transação de leitura
     public ProductDTO findById(Long id) {
-        Optional<Product> result = repository.findById(id);
-        Product product = result.get();
+        Product product = repository.findById(id).get();
         return new ProductDTO(product);
     }
 
+    @Transactional(readOnly = true) // Indica que é uma transação de leitura
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> products = repository.findAll(pageable);
+        return products.map(x -> new ProductDTO(x));
+    }
 }
