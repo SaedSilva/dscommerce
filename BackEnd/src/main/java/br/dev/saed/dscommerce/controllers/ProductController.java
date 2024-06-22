e6acbfdf0b836c41e8617806f655a4a98c160089
Saed Sousa <saed.sousa@hotmail.com>
2024-06-21 21:31:27 -0300

    PostMapping Product

--- BackEnd/src/main/java/br/dev/saed/dscommerce/controllers/ProductController.java
+++ BackEnd/src/main/java/br/dev/saed/dscommerce/controllers/ProductController.java
@@ -5,7 +5,12 @@ import br.dev.saed.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
+
+import java.net.URI;
 
 // Anotações que ficam acima da declaração da classe
 @RestController // Indica que a classe é um controlador REST
@@ -17,18 +22,20 @@ public class ProductController {
 
     // Anotações que ficam acima da declaração do método
     @GetMapping(value = "/{id}") // Indica que o método responde a requisições GET em /products/{id}
-    public ProductDTO findById(@PathVariable Long id) { // @PathVariable indica que o parâmetro vem da URL
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) { // @PathVariable indica que o parâmetro vem da URL
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok().body(service.findAll(pageable));
     }
 
     @PostMapping // Indica que o método responde a requisições POST
-    public ProductDTO insert(@RequestBody ProductDTO dto) { // @RequestBody indica que o parâmetro vem no corpo da requisição
-        return service.insert(dto);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) { // @RequestBody indica que o parâmetro vem no corpo da requisição
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri(); // Cria a URI do novo recurso
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
